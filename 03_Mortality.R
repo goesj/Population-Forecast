746ec0bd66a2eca9b6d579858dbdbb56fdecc442
’goesj’ <goesjulius@gmail.com>
2025-06-26 11:24:18 +0200

    add comments and packages

--- 03_Mortality.R
+++ 03_Mortality.R
@@ -1,10 +1,11 @@
-pacman::p_load("tidyverse","openxlsx","rstan","INLA","spdep")
+#### SCRIPT TO RUN MORTALITY MODEL ############################################
+pacman::p_load("tidyverse","rstan","spdep","reshape2")
 
 
 options(mc.cores = parallel::detectCores())
-source("02_Functions.R")
+source("02_Functions.R") #load helper functions
 
-load(file = file.path(getwd(),"Data/TotalData.RData"))
+load(file = file.path(getwd(),"Data/TotalData.RData")) #load data
 
 ###### Combine Death data with respective population data #########
 DeathData <- left_join(x = DeathCounts, 
@@ -28,10 +29,10 @@ scalingFac <- ScalingFacBYM2(Nodes = Nodes, AdjMat = AdjMatOF)
 
 
 ### 01_ Females ################################################################
-FemaleDeaths <- DeathData %>% 
+FemaleDeaths <- DeathData %>% #Get data
   filter(Sex == "female") 
 
-
+#create stan data
 StanData <- list("T" = max(FemaleDeaths$YearID),
                  "A" = max(FemaleDeaths$AgeID_New),
                  "R" = max(FemaleDeaths$RegionID), 
@@ -45,16 +46,18 @@ StanData <- list("T" = max(FemaleDeaths$YearID),
                  "node1" = Nodes$node1, "node2" = Nodes$node2,
                  "scaling_factor" = scalingFac)
 
+#run model (attention, may take a while)
 RH_BYM2_Female <- rstan::stan(file = file.path(getwd(),"StanCode/RH_BYM2.stan"),
                               data = StanData, 
                               chains = 4, iter = 4000, warmup = 2000, thin = 2, 
                               control = list("max_treedepth" = 12,
                                              "adapt_delta" = 0.82))
 
-
+#save results 
 save(RH_BYM2_Female, file = file.path(getwd(),"Results/MortRate_F_RH_BYM2.RData"))
 
-# extract forecasts and transform results into array for population projection 
+# extract forecasts and transform results into array for population projection##
+#Note, Results are not found in Git Repo since they are too big. 
 load(file = file.path(getwd(),"Results/MortRate_F_RH_BYM2.RData"))
 
 Mort_Rate_FC_F_RH <- rstan::extract(RH_BYM2_Female, pars ="mufor")$mufor
@@ -66,15 +69,16 @@ MortRateHelperData <- expand.grid("AgeGroup" = unique(PopCounts$AgeGroup_New),
 #Transform Mortality Results into Array for forecasting
 Mort_Rate_Array_F <- 
   Mort_Rate_FC_F_RH %>% t() %>% as.data.frame() %>% 
-  mutate("Year" = MortRateHelperData$Year,#add information
+  mutate("Year" = MortRateHelperData$Year, #add information
          "RegionNumber" = MortRateHelperData$Region, 
          "AgeGroup" = MortRateHelperData$AgeGroup, 
          .before = 1) %>% 
-  pivot_longer(cols = 4:ncol(.), names_to ="Iteration", 
+  pivot_longer(cols = 4:ncol(.), names_to ="Iteration", #into long format
                values_to = "mu_for") %>% 
-  mutate("mu_for" = exp(mu_for)) %>% 
+  mutate("mu_for" = exp(mu_for)) %>% #exponentiate to get rates
   mutate(Iteration = as.numeric(gsub("V", "", Iteration))) %>% #extract the numbers
-  reshape2::acast(formula = AgeGroup ~ Year ~ RegionNumber ~ Iteration, value.var = "mu_for")
+  reshape2::acast(formula =  AgeGroup ~ Year ~ RegionNumber ~ Iteration, 
+                  value.var = "mu_for")
 
 
 #### 02_Males ##################################################################
@@ -122,7 +126,8 @@ Mort_Rate_Array_M <-
                values_to = "mu_for") %>% 
   mutate("mu_for" = exp(mu_for)) %>% 
   mutate(Iteration = as.numeric(gsub("V", "", Iteration))) %>% #extract the numbers
-  reshape2::acast(formula = AgeGroup ~ Year ~ RegionNumber ~ Iteration, value.var = "mu_for")
+  reshape2::acast(formula = AgeGroup ~ Year ~ RegionNumber ~ Iteration, 
+                  value.var = "mu_for")
 
 save(Mort_Rate_Array_M, 
      Mort_Rate_Array_F, 
